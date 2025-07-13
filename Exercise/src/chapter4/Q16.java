cf20ca70178a662859aad761a4078215aa994df1
easyspubjava <eunjong.park@gmail.com>
2025-07-13 14:05:07 +0900

    exercise

---
+++ Exercise/src/chapter4/Q16.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+public class Q16 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 0;
+
+        for (num = 1; ; num++) {
+            sum += num;
+            if (sum >= 500)
+                break;
+        }
+        System.out.println("num : " + num);
+        System.out.println("sun : " + sum);
+    }
+}
+
